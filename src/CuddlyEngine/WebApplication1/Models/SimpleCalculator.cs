52442ec19a18da1cc7f93dffa64b15c06e40f627
Collin <96417832+Kolumnist@users.noreply.github.com>
2023-08-20 23:46:06 +0200

    Update Calculator to SimpleCalculator using too much javascript
    Essentially I wanted to try to use C# with AJAX but POST requests hate me so I will at least for the simple calculator, use javascript and not the Model I just made.

---
+++ src/CuddlyEngine/WebApplication1/Models/SimpleCalculator.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+	public class SimpleCalculator
+	{
+		public string? calculation { get; set; }
+		public decimal result { get; set; } = 0;
+
+		public static void Calculate() 
+		{
+
+		}
+	}
+}
---
+++ src/CuddlyEngine/WebApplication1/Pages/Calculator/SimpleCalculator.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages.Calculator
+{
+    public class CalculatorModel : PageModel
+    {
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToString("d", new CultureInfo("de-DE"));
+            ViewData["TimeStamp"] = dateTime;
+        }
+	}
+}
